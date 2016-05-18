47214039fc1e992131cc7efdb07dd28e02602b38
yanzhou <yanzhou@YANZHOU22>
2016-05-18 12:06:57 +0800

    1

--- Young.Data/Utils.cs
+++ Young.Data/Utils.cs
@@ -57,5 +57,42 @@ namespace Young.Data
             }
             return table;
         }
+
+        public static DataTable ReadStringToTable(string filePath, Func<string, string,DataTable, List<string>> LineFunc) {
+            string tempString = "";
+            DataTable table = null;
+            string headerRow = "";
+            using (StreamReader sr = new StreamReader(filePath)) {
+                while (!sr.EndOfStream) {
+                    tempString = sr.ReadLine();
+                    var vals = LineFunc(tempString, headerRow,table);
+                    if (vals != null && vals.Count() > 0) {
+                        if (table == null) {
+                            table = new DataTable();
+                            Dictionary<string, int> cols = new Dictionary<string, int>();
+                            headerRow = tempString;
+                            for (int i = 0; i < vals.Count(); i++) {
+                                if (vals[i] == "") {
+                                    vals[i] = "Header_Temp_" + i.ToString();
+                                }
+                                while (cols.ContainsKey(vals[i])) {
+                                    vals[i] = vals[i] + "1";
+                                }
+                                cols.Add(vals[i], 0);
+
+                                table.Columns.Add(vals[i]);
+                            }
+                        } else {
+                            DataRow dr = table.NewRow();
+                            for (int i = 0; i < vals.Count(); i++) {
+                                dr[i] = vals[i];
+                            }
+                            table.Rows.Add(dr);
+                        }
+                    }
+                }
+            }
+            return table;
+        }
     }
 }
